4abc8382d99fcab602e4488bfc5dc81f0307a555
Justin Johnson <jjustin634@gmail.com>
2021-11-13 22:31:44 -0600

    Deploy (#21)
    * first commit
    
    * travis setup
    
    * branch for travis deployment
    
    * circle ci setup
    
    * initial setup
    
    * circleci
    
    * circleci
    
    * circleci
    
    * Build testing (#5)
    
    * circleci
    
    * circleci
    
    * circleci
    
    * Build testing (#7)
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * Build testing (#9)
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * Build testing (#11)
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * Rename app.yml to app.yaml
    
    * Update app.yaml
    
    * Update app.yaml
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * circleci
    
    * user service setup (#16)
    
    * Discord build notifications (#17)
    
    * only build and deploy main branch
    
    * add discord notifications for deploys
    
    * remove custom server (#18)
    
    * auth0 setup (#19)
    
    * remove custom server
    
    * auth0 setup
    
    * adding the checkin api
    
    * some cleanup
    
    Co-authored-by: Justin Johnson <jjustin634@gmail.com>
    
    * adding tailwindcss and restyling main screen
    
    Co-authored-by: Eric Lee <ayrocklee@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { UserProvider } from "@auth0/nextjs-auth0";
+
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/api/checkin/index.js
@@ -0,0 +1,26 @@
+import { getAllCheckinsForUser, createCheckin } from "../../../services/checkin";
+
+export default function handler(req, res) {
+  const { method } = req;
+
+  if (method === "GET") {
+    const checkins = getAllCheckinsForUser(req.query.id);
+
+    if (checkins) {
+      res.status(200).json({ checkins });
+    } else {
+      res.status(404).send();
+    }
+  } else if (method === "POST") {
+
+    const id = createCheckin(req.query.id);
+
+    if (id) {
+      res.status(200).send(id.toString());
+    } else {
+      res.status(400).send();
+    }
+  } else {
+    res.status(405).send();
+  }
+}
