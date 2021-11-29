ffed48f9b42493fb3c6ba416aec3ad3fdd1e3c51
Justin Johnson <jjustin634@gmail.com>
2021-11-29 13:08:04 -0600

    Basic pages (#30)
    * adding footer component
    
    * encapsulating footer in its own component
    
    * adding basic pages for buttons

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
-import Nav from '../nav';
+import Nav from "../nav";
+import Footer from "../footer";
 
 export default function Layout({ children }) {
   return (
@@ -8,23 +7,10 @@ export default function Layout({ children }) {
       <Nav />
       <div className="container mx-auto">
         <main className="flex flex-col flex-1 justify-center items-center min-h-screen">
-          { children }
+          {children}
         </main>
       </div>
-      <footer className="flex p-4 px-1 bg-green-600 border-t justify-center items-center">
-        <Link
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <a className="flex justify-center items-center flex-grow">
-            Powered by{" "}
-            <span className="h-4 ml-2">
-              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </span>
-          </a>
-        </Link>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ pages/club/index.js
@@ -0,0 +1,3 @@
+export default function ClubWellness() {
+  return <div>Basic wellness screen</div>;
+}
