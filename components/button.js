cd09818ac1b40c4c0b42e8202bf071eac81f0f09
Justin Johnson <jjustin634@gmail.com>
2021-12-12 21:58:15 -0600

    updating button style

--- components/button.js
+++ components/button.js
@@ -1,7 +1,7 @@
 export default function Button(props) {
   return (
     <button
-      className="bg-black text-white p-2 rounded-lg font-mono"
+      className="bg-black text-white p-2 border rounded font-mono"
       onClick={props.handleClick}
     >
       {props.buttonName}
