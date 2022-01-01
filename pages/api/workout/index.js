566d424a60f045fa2cd132192efac6d34dde9ac0
Justin Johnson <jjustin634@gmail.com>
2022-01-01 17:24:57 -0600

    building method for posting to workout collection

--- pages/api/workout/index.js
+++ pages/api/workout/index.js
@@ -1,11 +1,17 @@
-import { getAllWorkouts } from '../../../services/workout';
+import { getAllWorkouts, createWorkout } from '../../../services/workout';
 
 export default async function handler(req, res) {
-  const { method } = req;
+  const { method, body } = req;
 
+  // get all workouts
   if (method === 'GET') {
-    
-    res.status(200).send(await getAllWorkouts());
+    const workouts = await getAllWorkouts();
+    workouts ? res.status(200).send(workouts) : res.status(404).send();
   }
 
+  // create workout
+  if (method === 'POST') {
+    const response = await createWorkout(body);
+    response ? res.status(200).send(response) : res.status(400).send();
+  }
 }
