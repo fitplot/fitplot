74c843c89ff3d75b29dfbf4dbcd9e489e248b021
Justin Johnson <jjustin634@gmail.com>
2022-01-01 21:50:53 -0600

    adding routes for Create/Read exercises/workouts/sets

---
+++ pages/api/workout/[workoutId]/sets.js
@@ -0,0 +1,15 @@
+import { getSetByWorkoutId, createSetForWorkout } from '../../../../services/set';
+
+export default async function handler(req, res) {
+  const { method, body, query: { workoutId } } = req;
+
+  if (method === 'GET') {
+    return res.status(200).send(await getSetByWorkoutId(workoutId));
+  }
+
+  if (method === 'POST') {
+    return res.status(200).send(await createSetForWorkout(body));
+  }
+
+  return res.status(405).send();
+}
