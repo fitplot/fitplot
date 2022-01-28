f7b18f19dc55f1f324b1d1a7919fc04836b53803
Justin Johnson <jjustin634@gmail.com>
2022-01-27 23:02:09 -0600

    creates workout, then assigns url to new workout object

--- src/hooks/use-workouts.js
+++ src/hooks/use-workouts.js
@@ -1,7 +1,27 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from "react-query";
+import queryClient from "../lib/query-client";
 
 export function useWorkouts() {
   return useQuery("workouts", () =>
-    fetch("/api/workouts").then((res) => res.json())
+    fetch("/api/workouts").then(res => res.json())
+  );
+}
+
+export function useCreateWorkout() {
+  return useMutation(
+    workout =>
+      fetch("/api/workouts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(workout)
+      }).then(res => res.json()),
+    {
+      onSuccess: res => {
+        queryClient.invalidateQueries("workouts");
+        window.location.assign(`/workout/${res.id}`);
+      }
+    }
   );
 }
