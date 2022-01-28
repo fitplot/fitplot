ca9cd9b88d5ed04f3d3013bb28e93d0d391e6598
Justin Johnson <jjustin634@gmail.com>
2022-01-27 21:54:39 -0600

    removing useUser() hook in layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,14 +1,6 @@
-import React from "react";
 import Nav from "../nav";
-import { useUser } from "../auth";
 
 export default function Layout({ children }) {
-  const { login } = useUser();
-
-  React.useEffect(() => {
-    login("justin");
-  }, []);
-
   return (
     <>
       <div className="flex flex-col md:flex-row md:flex-row-reverse w-screen h-screen bg-white">
