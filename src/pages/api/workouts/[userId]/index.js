bf26828d863b430f3769d9e4503d93a4227f3a51
Justin Johnson <jjustin634@gmail.com>
2022-01-28 21:46:49 -0600

    adding server changes to fetch workouts by user id

---
+++ src/pages/api/workouts/[userId]/index.js
@@ -0,0 +1,11 @@
+import { getWorkoutsByUserId } from '../../../../services/workout';
+
+export default async function handler(req, res) {
+  const { method, query: { userId } } = req;
+
+  if (method === 'GET') {
+    return res.status(200).send(await getWorkoutsByUserId(userId));
+  }
+
+  return res.status(405).send();
+}
