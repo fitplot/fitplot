62ddf9520a0d5c26ad142f88990fa95a66242d26
Justin Johnson <jjustin634@gmail.com>
2022-01-28 21:04:41 -0600

    Adding modal for user creating a workout (#58)
    * adding name to workout collection schema
    
    * updating my name to be shorter for auth
    
    * removing controlled component to uncontrolled
    
    * adding AddWorkout component for opening dialog and creating workout name
    
    * adding modal for adding a workout

--- src/components/workout/MyWorkouts.js
+++ src/components/workout/MyWorkouts.js
@@ -4,59 +4,56 @@ import Button from "../button";
 import Card from "../card";
 import Layout from "../layout";
 import LoadingIcon from "../loading-icon";
-import { useWorkouts, useCreateWorkout } from "../../hooks/use-workouts";
+import { useWorkouts } from "../../hooks/use-workouts";
 import { useRouter } from "next/router";
 import { ChevronRightIcon, FireIcon } from "@heroicons/react/solid";
 import dayjs from "dayjs";
-import { useUser } from "../auth/UserProvider";
+import AddWorkout from "./AddWorkout";
 
 export default function MyWorkouts() {
   const router = useRouter();
   const { data: workouts, error, isLoading } = useWorkouts();
-  const { user } = useUser();
-  const mutation = useCreateWorkout();
 
-  const newWorkout = async () => {
-    await mutation.mutateAsync({ userId: user.id });
+  const [showExerciseDialog, setShowExerciseDialog] = React.useState(false);
+  const openExerciseDialog = () => setShowExerciseDialog(true);
+  const closeExerciseDialog = () => {
+    setShowExerciseDialog(false);
   };
 
   return (
     <Layout>
-      <div className="flex-1 flex flex-col space-y-4">
-        <Button
-          className="inline-flex justify-center items-center"
-          onClick={newWorkout}
-        >
+      <div className="flex flex-col space-y-4">
+        <Button className="inline-flex justify-center items-center" type="submit" onClick={openExerciseDialog}>
           <FireIcon className="w-6 h-6" />
           Workout Now
         </Button>
-        <H1>My Workouts</H1>
-        <div className="flex-1 flex flex-col justify-center">
-          {isLoading && <LoadingIcon className="w-12 h-12 self-center" />}
-          {!isLoading &&
-            !error &&
-            (workouts ? (
-              workouts.map(({ id, createdAt }) => (
-                <Card key={id} className="flex mb-4 border border-slate-200">
-                  <div className="flex-1 p-4">
-                    {/* TODO: slap a name field on these workouts so we dont have to show ID */}
-                    <div className="text-sm font-medium text-slate-900">
-                      {id}
-                    </div>
-                    <div className="text-sm font-medium text-slate-500">
-                      {dayjs(createdAt).format("MMM DD, YYYY h:mm a")}
-                    </div>
-                  </div>
-                  <Button onClick={() => router.push(`/workout/${id}`)}>
-                    <ChevronRightIcon className="w-6 h-6" />
-                  </Button>
-                </Card>
-              ))
-            ) : (
-              <Paragraph>No workout history.</Paragraph>
-            ))}
-        </div>
       </div>
-    </Layout>
+      <AddWorkout isOpen={showExerciseDialog} close={closeExerciseDialog} />
+      <H1>My Workouts</H1>
+      <div className="flex-1 flex flex-col justify-center">
+        {isLoading && <LoadingIcon className="w-12 h-12 self-center" />}
+        {!isLoading &&
+          !error &&
+          (workouts ? (
+            workouts.map(({ id, name, createdAt }) => (
+              <Card key={id} className="flex mb-4 border border-slate-200">
+                <div className="flex-1 p-4">
+                  <div className="text-sm font-medium text-slate-900">
+                    {name}
+                  </div>
+                  <div className="text-sm font-medium text-slate-500">
+                    {dayjs(createdAt).format("MMM DD, YYYY h:mm a")}
+                  </div>
+                </div>
+                <Button onClick={() => router.push(`/workout/${id}`)}>
+                  <ChevronRightIcon className="w-6 h-6" />
+                </Button>
+              </Card>
+            ))
+          ) : (
+            <Paragraph>No workout history.</Paragraph>
+          ))}
+      </div>
+    </Layout >
   );
 }
