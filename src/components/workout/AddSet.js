67bad2c77d1bc9f67a990804844d1a0297b9bb29
Justin Johnson <jjustin634@gmail.com>
2022-01-31 16:37:14 -0600

    Fitcode display button (#64)
    * adding chevron-right image to enable fitCode
    
    * adjusting properties
    
    * using proper import for ChevronRightIcon

--- src/components/workout/AddSet.js
+++ src/components/workout/AddSet.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Dialog from "@reach/dialog";
 import { Input, Label } from "../forms";
+import { ChevronRightIcon } from "@heroicons/react/solid";
 import SetsView from "./SetsView";
 import Button from "../button";
 import { H2 } from "../typography";
@@ -51,12 +52,17 @@ export default function AddSet({ isOpen, close, workoutId, exerciseId }) {
             <div className="flex flex-wrap items-baseline justify-between mb-4">
               <Label htmlFor="exercise-name">Type your FitCode™</Label>
             </div>
-            <Input
-              ref={inputRef}
-              type="text"
-              name="fitcode"
-              placeholder="5@185, 4@195, 2@205"
-            />
+            <div className="flex">
+              <Input
+                ref={inputRef}
+                type="text"
+                name="fitcode"
+                placeholder="5@185, 4@195"
+              />
+              <Button className="p-1">
+                <ChevronRightIcon className="w-6 h-6" />
+              </Button>
+            </div>
           </div>
         </form>
         <div className="flex-1 p-4">
