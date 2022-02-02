e1508987887d001f0286bf3893424795abc783eb
Justin Johnson <jjustin634@gmail.com>
2022-02-01 18:43:14 -0600

    renaming req id

--- src/pages/api/exercises/[userId]/index.js
+++ src/pages/api/exercises/[userId]/index.js
@@ -1,10 +1,10 @@
 import { getExercisesByUserId } from '../../../../services/exercise';
 
 export default async function handler(req, res) {
-  const { method, query: { exerciseId } } = req;
+  const { method, query: { userId } } = req;
 
   if (method === 'GET') {
-    return res.status(200).send(await getExercisesByUserId(exerciseId));
+    return res.status(200).send(await getExercisesByUserId(userId));
   }
 
   return res.status(405).send();
