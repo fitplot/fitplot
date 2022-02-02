48dfddd557c442a97c0f7a0bd9406409c60b6332
Justin Johnson <jjustin634@gmail.com>
2022-02-01 18:32:23 -0600

    calling comonent from page directory

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,15 +1,6 @@
-import Layout from "../../components/layout";
-import { H1 } from "../../components/typography";
-import { useUser } from "../../components/auth";
-import Button from "../../components/button";
+import Profile from '../../components/profile';
 
-export default function Profile() {
-  const { logout } = useUser();
-
-  return (
-    <Layout>
-      <H1>Profile</H1>
-      <Button onClick={() => logout()}>Logout</Button>
-    </Layout>
-  );
+export default function ProfilePage() {
+  return <Profile />;
 }
+
