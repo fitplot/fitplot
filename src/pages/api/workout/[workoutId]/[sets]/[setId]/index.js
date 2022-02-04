91812b34ebdfb1f19eaffd12f7aa4cfdf7fc8b6e
Justin Johnson <jjustin634@gmail.com>
2022-02-04 08:18:37 -0600

    update query on workoutset

---
+++ src/pages/api/workout/[workoutId]/[sets]/[setId]/index.js
@@ -0,0 +1,14 @@
+import { updateSetForWorkout } from "../../../../../../services/set";
+
+export default async function handler(req, res) {
+  const { method, body } = req;
+
+  if (method === "PUT") {
+    const { id, amount, unit, volume } = body;
+    return res
+      .status(200)
+      .send(await updateSetForWorkout(id, amount, unit, volume));
+  }
+
+  return res.status(405).send();
+}
