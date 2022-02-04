066c6cf77b158868381f0a707c17093072d35742
Justin Johnson <jjustin634@gmail.com>
2022-02-04 08:18:22 -0600

    formatting

--- src/pages/api/workout/[workoutId]/index.js
+++ src/pages/api/workout/[workoutId]/index.js
@@ -1,9 +1,12 @@
-import { getWorkoutById } from '../../../../services/workout';
+import { getWorkoutById } from "../../../../services/workout";
 
 export default async function handler(req, res) {
-  const { method, query: { workoutId } } = req;
+  const {
+    method,
+    query: { workoutId }
+  } = req;
 
-  if (method === 'GET') {
+  if (method === "GET") {
     return res.status(200).send(await getWorkoutById(workoutId));
   }
 
