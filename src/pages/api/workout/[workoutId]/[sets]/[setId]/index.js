ecae8b35694edbdcb0cf99871925a97e4e2da78a
Justin Johnson <jjustin634@gmail.com>
2022-02-06 17:41:12 -0600

    Update src/pages/api/workout/[workoutId]/[sets]/[setId]/index.js
    suggestion: pass these in together so the list of arguments doesn't grow as we add fields!
    
    Co-authored-by: Eric Lee <ayrocklee@gmail.com>

--- src/pages/api/workout/[workoutId]/[sets]/[setId]/index.js
+++ src/pages/api/workout/[workoutId]/[sets]/[setId]/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     const { id, amount, unit, volume } = body;
     return res
       .status(200)
-      .send(await updateWorkoutSet(id, amount, unit, volume));
+      .send(await updateWorkoutSet(id, { amount, unit, volume, }));
   }
 
   return res.status(405).send();
