c86fb9900865234176165f8dc827eea80f8ab2c0
Justin Johnson <jjustin634@gmail.com>
2022-02-15 22:08:37 -0600

    Edit workout name (#89)
    * edit workout name api
    
    * empty commit

--- src/pages/api/workout/[workoutId]/index.js
+++ src/pages/api/workout/[workoutId]/index.js
@@ -1,13 +1,19 @@
-import { getWorkoutById } from "../../../../services/workout";
+import { getWorkoutById, updateWorkout } from '../../../../services/workout';
 
 export default async function handler(req, res) {
   const {
     method,
+    body,
     query: { workoutId },
   } = req;
 
-  if (method === "GET") {
+  if (method === 'GET') {
     return res.status(200).send(await getWorkoutById(workoutId));
+  }
+
+  if (method === 'PUT') {
+    const { id, name } = body;
+    return res.status(200).send(await updateWorkout(id, { name }));
   }
 
   return res.status(405).send();
