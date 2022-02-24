a1bcc973fce207f0df47f7522ad69aefa00ef59f
Eric Lee <ayrocklee@gmail.com>
2022-02-23 17:10:03 -0800

    change the PWA theme color from red to the bg grey

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,7 +20,8 @@ function App({ Component, pageProps }) {
             <meta name='description' content='Nexus Fitness' />
             <link rel='icon' href='/favicon.ico' />
             <meta name='author' content='Nexus Fitness' />
-            <meta name='theme-color' content='#B12A34' />
+            {/* slate-50 */}
+            <meta name='theme-color' content='rgb(241 245 249)' />
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta property='og:image' content='/favicon.ico' />
           </Head>
