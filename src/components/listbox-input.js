2f909169cf480af9b5599b298b6109f0caff8ff0
Eric Lee <ayrocklee@gmail.com>
2023-02-26 20:45:14 -0800

    use @headlessui Combobox instead of the custom listbox

--- src/components/listbox-input.js
+++ src/components/listbox-input.js
@@ -1,81 +1,74 @@
+import { Combobox } from '@headlessui/react';
 import { EllipsisVerticalIcon } from '@heroicons/react/24/solid';
 import _ from 'lodash';
 import React from 'react';
 
 import { Input } from './forms';
 
-export default function ListboxInput({
-  options = [],
+export default function CustomCombobox({
+  options,
+  keyField = 'id',
+  field,
+  onAdd,
   onSelect,
-  onSubmit,
-  field = null,
-  predicate,
-  exactPredicate = () => _.identity,
-  ...inputProps
 }) {
-  const [value, setValue] = React.useState('');
+  const [query, setQuery] = React.useState('');
 
-  const onChange = (rawInput) => {
-    setValue(rawInput.trim());
-  };
-
-  const filteredOptions = React.useMemo(() => {
-    const filter =
-      predicate ||
-      ((input) => (field ? _.matches({ [field]: input }) : _.identity));
-
-    return value ? options.filter(filter(value)) : [];
-  }, [options, value, predicate, field]);
+  const filteredOptions = React.useMemo(
+    () =>
+      query
+        ? options.filter((option) =>
+            (field ? option[field] : option)
+              .toLowerCase()
+              .includes(query.toLowerCase())
+          )
+        : [],
+    [options, query, field]
+  );
 
   const hasExactMatch = React.useMemo(
-    () => options.some(exactPredicate(value)),
-    [options, value, exactPredicate]
+    () =>
+      options.some(
+        (option) =>
+          query.toLowerCase() === (field ? option[field] : option).toLowerCase()
+      ),
+    [options, query, field]
   );
 
   return (
-    <div className='flex flex-col space-y-2 h-full'>
-      <div className='grow-0 shrink-0'>
-        <Input
-          onChange={(event) => onChange(event.target.value)}
-          {...inputProps}
-        />
-      </div>
-      <div className='flex overflow-y-auto flex-col grow pt-2 pb-4 bg-slate-100 divide-y'>
-        <span className='py-3 px-6 text-xs font-medium tracking-wider text-slate-500 uppercase'>
-          Select an exercise or create one
-        </span>
+    <Combobox>
+      <Combobox.Input
+        as={Input}
+        onChange={(event) => setQuery(event.target.value)}
+        displayValue={(option) => (field ? option[field] : option)}
+      />
+      <Combobox.Options className='flex overflow-y-auto flex-col grow pt-2 pb-4 bg-slate-100 divide-y'>
         {filteredOptions.map((option) => (
-          <ListboxOption
-            displayValue={_.get(option, field, option)}
-            onSelect={() => onSelect(option)}
-          />
+          <Combobox.Option
+            key={keyField ? option[keyField] : option}
+            value={option}
+            as='button'
+            className='flex items-center p-2 bg-white'
+            type='button'
+            onClick={() => onSelect(option)}
+          >
+            <EllipsisVerticalIcon className='inline-block w-5 h-5 text-slate-500' />
+            <span>{field ? option[field] : option}</span>
+          </Combobox.Option>
         ))}
-        {!hasExactMatch && value && (
-          <ListboxOption
-            displayValue={value}
-            onSelect={() => onSubmit(value)}
-            isNew
-          />
+        {Boolean(onAdd) && query && !hasExactMatch && (
+          <Combobox.Option
+            as='button'
+            type='button'
+            onClick={() => onAdd(query)}
+            className='flex items-center p-2 bg-white'
+          >
+            <EllipsisVerticalIcon className='inline-block w-5 h-5 text-slate-500' />
+            <span className='pr-2 text-slate-500'>Create</span>
+            <span>{query}</span>
+          </Combobox.Option>
         )}
-      </div>
-    </div>
-  );
-}
-
-function ListboxOption({ displayValue, onSelect, ...props }) {
-  const isNew = Object.prototype.hasOwnProperty.call(props, 'isNew');
-
-  return (
-    <button
-      type='button'
-      className='flex items-center p-2 bg-white'
-      onClick={onSelect}
-    >
-      {!isNew && (
-        <EllipsisVerticalIcon className='inline-block w-5 h-5 text-slate-500' />
-      )}
-      {isNew && <span className='pr-2 text-slate-500'>Create</span>}
-      <span className='font-medium'>{displayValue}</span>
-    </button>
+      </Combobox.Options>
+    </Combobox>
   );
 }
--- src/components/workout/overlays/add-exercise.js
+++ src/components/workout/overlays/add-exercise.js
@@ -2,58 +2,36 @@ import React from 'react';
 
 import { useCreateExercise, useExercises } from '../../../hooks/use-exercises';
 import { Label } from '../../forms';
-import ListboxInput from '../../listbox-input';
+import Combobox from '../../listbox-input';
 import LoadingIcon from '../../loading-icon';
 import Overlay from '../../overlay';
 
-const predicate = (input) => (exercise) =>
-  exercise.name.toLowerCase().includes(input.toLowerCase());
-const exactPredicate = (input) => (exercise) =>
-  exercise.name.toLowerCase() === input.toLowerCase();
-
 export default function AddExercise({ open, onClose }) {
-  const inputRef = React.useRef(null);
-
   const { data: exercises, ...query } = useExercises();
   const mutation = useCreateExercise();
 
-  const onSelect = (exercise) => {
-    onClose(exercise.id);
-  };
+  const isLoading = query.isLoading || mutation.isLoading;
 
-  const onSubmit = async (name) => {
+  const onAdd = async (name) => {
     const exercise = await mutation.mutateAsync({ name });
     onClose(exercise.id);
   };
 
-  const isLoading = query.isLoading || mutation.isLoading;
+  const onSelect = (exercise) => onClose(exercise.id);
 
   return (
-    <Overlay
-      title='Add Exercise'
-      open={open}
-      onClose={onClose}
-      initialFocus={inputRef}
-    >
+    <Overlay title='Add Exercise' open={open} onClose={onClose}>
       {isLoading && <LoadingIcon className='w-5 h-5' />}
       {!isLoading && (
         <div className='flex flex-col p-4 space-y-4 h-full'>
           <div className='flex flex-wrap grow-0 shrink-0 justify-between items-baseline'>
             <Label htmlFor='name'>Exercise name</Label>
           </div>
-          <ListboxInput
+          <Combobox
             options={exercises}
             field='name'
-            predicate={predicate}
-            exactPredicate={exactPredicate}
+            onAdd={onAdd}
             onSelect={onSelect}
-            onSubmit={onSubmit}
-            autoFocus
-            type='text'
-            id='name'
-            name='name'
-            required
-            ref={inputRef}
           />
         </div>
       )}
