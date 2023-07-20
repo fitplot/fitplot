e7da8b65e933452290ef7415cf465e4e2bb19a83
Eric Lee <ayrocklee@gmail.com>
2023-07-19 20:22:43 -0700

    /welcome title typo

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -7,7 +7,7 @@ import { H3, Paragraph } from '@/components/typography';
 import { Button } from '@/components/ui/button';
 
 export default function Welcome() {
-  usePageContext({ title: 'Welcome ' });
+  usePageContext({ title: 'Welcome' });
 
   const router = useRouter();
   const [theme, setTheme] = React.useState('light');
